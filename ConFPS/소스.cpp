f59f7146c2ac89c2a3d489544538522cfc2cc59b
kth <0307rnxogh@daum.net>
2023-12-29 19:17:08 +0900

    ConsoleFPS.develop

--- ConFPS/소스.cpp
+++ ConFPS/소스.cpp
@@ -1,8 +1,20 @@
+#include<iostream>
+#include<Windows.h>
+
 using namespace std;
 
+int nScreenWidth = 120;
+int nScreenHeight = 40;
 
 int main() {
-	
+	//스크린 버퍼 만들기
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	screen[nScreenWidth * nScreenHeight - 1] = '\0';
+	WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 
 	return 0;
 }
